ac33543502a9221c061abcaf41b78f37e94bc5cf
Nithish Karanam <nithishkaranam@example.com>
2026-04-22 11:25:49 -0500

    Fix jinja2 and gradio versions

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import sys
 import os
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-port = int(os.environ.get("PORT", 7860))
+os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0"
+os.environ["GRADIO_SERVER_PORT"] = str(os.environ.get("PORT", "7860"))
 
 from frontend.app import demo
 
 demo.launch(
     server_name="0.0.0.0",
-    server_port=port,
+    server_port=int(os.environ.get("PORT", 7860)),
     share=False,
     show_error=True
 )
