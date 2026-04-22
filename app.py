6651beca81fd965d37482733d46d1ef5672ffaea
Nithish Karanam <nithishkaranam@example.com>
2026-04-22 00:34:02 -0500

    Fix launch settings for deployment

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from frontend.app import demo
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
